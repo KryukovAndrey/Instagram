2f57480d3f032f2f30f5cbff4503d0411742a8db
KryukovAndrey <kryukov.ios@yandex.ru>
2022-09-27 12:07:20 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Andrey Kryukov on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
